535ac71fa23be9c82b8ebba057cda6ade04fafef
Murphy <matt.griffiths@gmail.com>
2020-01-31 11:17:30 -0800

    Create os-tools.sh

---
+++ os-tools.sh
@@ -0,0 +1,19 @@
+# /bin/bash
+
+mkdir Linux
+mkdir Windows
+cd Linux
+git clone https://github.com/TH3xACE/SUDO_KILLER.git
+wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh
+wget https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/master/lse.sh
+https://raw.githubusercontent.com/sleventyeleven/linuxprivchecker/master/linuxprivchecker.py
+
+cd ..
+cd Windows
+git clone https://github.com/PowerShellMafia/PowerSploit.git
+git clone https://github.com/AlessandroZ/BeRoot.git
+https://raw.githubusercontent.com/AonCyberLabs/Windows-Exploit-Suggester/master/windows-exploit-suggester.py
+git clone https://github.com/pentestmonkey/windows-privesc-check.git
+https://raw.githubusercontent.com/411Hall/JAWS/master/jaws-enum.ps1
+https://raw.githubusercontent.com/M4ximuss/Powerless/master/Powerless.bat
+
